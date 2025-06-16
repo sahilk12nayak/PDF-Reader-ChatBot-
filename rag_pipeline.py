836acca45fdb76ba7f55d89ae622a9dbf86355a8
Sahil Kumar Nayak <sahilnayak2812@gmail.com>
2025-06-17 02:31:40 +0530

    Update rag_pipeline.py

--- rag_pipeline.py
+++ rag_pipeline.py
@@ -31,21 +31,3 @@ def build_rag_chain(persist_dir="db"):
 
 
 
-
-
-
-
-
-# from langchain.vectorstores import Chroma
-# from langchain.embeddings import HuggingFaceEmbeddings
-# from langchain.llms import OpenAI
-# from langchain.chains import RetrievalQA
-
-# def build_rag_chain(persist_dir="db"):
-#     embeddings = HuggingFaceEmbeddings(model_name="all-MiniLM-L6-v2")
-#     vectordb = Chroma(persist_directory=persist_dir, embedding_function=embeddings)
-#     retriever = vectordb.as_retriever(search_kwargs={"k": 3})
-
-#     llm = OpenAI(temperature=0.2)
-#     qa_chain = RetrievalQA.from_chain_type(llm=llm, retriever=retriever, return_source_documents=True)
-#     return qa_chain
