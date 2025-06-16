6663d54c9d3214c279391abf4e5db20685bff611
Sahil Kumar Nayak <sahilnayak2812@gmail.com>
2025-06-17 02:30:40 +0530

    Update ingest.py

--- ingest.py
+++ ingest.py
@@ -26,28 +26,3 @@ if __name__ == "__main__":
 
 
 
-
-
-
-
-
-
-# from langchain.document_loaders import PyPDFLoader, DirectoryLoader
-# from langchain.text_splitter import RecursiveCharacterTextSplitter
-# from langchain.embeddings import HuggingFaceEmbeddings
-# from langchain.vectorstores import Chroma
-
-# def ingest_documents(doc_dir="docs", persist_dir="db"):
-#     loader = DirectoryLoader(doc_dir, glob="*.pdf", loader_cls=PyPDFLoader)
-#     documents = loader.load()
-
-#     splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=100)
-#     splits = splitter.split_documents(documents)
-
-#     embeddings = HuggingFaceEmbeddings(model_name="all-MiniLM-L6-v2")
-#     vectordb = Chroma.from_documents(splits, embedding=embeddings, persist_directory=persist_dir)
-#     vectordb.persist()
-#     return vectordb
-
-# if __name__ == "__main__":
-#     ingest_documents()
